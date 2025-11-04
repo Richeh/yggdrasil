a7963401f34d291aa04bb82a34531a958a8e681d
Rich Gwilliam <rich.gwilliam@gmail.com>
2025-11-04 14:23:44 +0000

    Created user CRUD Implemented Sqlite database Detailed concept of project

--- index.js
+++ index.js
@@ -1,9 +1,69 @@
 const express=require('express');
+const bodyParser=require('body-parser');
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
 const app=express();
 const port=3000;
 
-app.get('/',(req, res)=> {
-    res.send('Hello World!');
+// Create sequelize instance
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
+
+// define user model
+
+class User extends Model {}
+User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+}, { sequelize, modelName: 'user'});
+
+// Sync models with database
+
+sequelize.sync();
+
+// Middleware for parsing request body
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// CRUD routes for User model
+app.get('/users/', async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
+});
+
+app.get('users/id', async  (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    res.json(user);
+});
+
+app.post('/users', async (req, res) => {
+    const user = await User.create(req.body);
+    res.json(user);
+});
+
+app.put('/users/id', async(req, res) => {
+    const user = await User.findByPk(req.params.id);
+    if(user) {
+        await user.update(req.body);
+        res.json(user);
+    } else {
+        res.status(404).send('User not found');
+    }    
+});
+
+app.delete('/users/id', async(req, res) => {
+    const user = await User.findByPk(req.params.id);
+    if(user) {
+        await user.destroy();
+        res.json({message: 'User deleted'});
+    } else {
+        res.status(404).send('User not found');
+    }
 });
 
 app.listen(port, () => {
--- readme.md
+++ readme.md
@@ -4,12 +4,22 @@ an idiotically titled family tree application
 
 ## Description
 
-This is an MIT licensed app to store and display your family tree.  
+    This is an MIT licensed app to store and display your family tree.  I'm using this to learn Express, so initially a lot of this may be... bad.
 
 ## Installation / running
 
-* Git clone
-* npm install
-* configure using settings currently in index.js (TODO: move this.)
-* node index.js
-* access on localhost/3000 (If running locally using default port)
+    * Git clone
+    * npm install
+    * configure using settings currently in index.js (TODO: move this.)
+    * node index.js
+    * access on localhost/3000 (If running locally using default port)
+
+## Concept
+
+    * A tree has a list of people.  
+    * Each person has a birth date and a death date - both can be optional to allow for living people and incomplete data.
+    * Each person has parents, represented by a parental link to another person.  
+    * A parental link can be biological (born to / concieved by) and / or civic (adopted by / raised by).  
+    * There is no need to limit the number of parental connections.  Families are complicated.
+    * Sibling connections can be inferred.
+    * More complex data can be stored on a lookup table with an enum "type" - this might need to be rethought depending on performance, but allows flex for now.
